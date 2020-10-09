d5366677359c6aea759460ca47e68e622c0ba880
unknown <judithportocarrero99@gmail.com>
2020-10-08 20:20:39 -0500

    Updates

---
+++ precache-manifest.f31fc23d521ebd5f65ea88335518cfbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ee8b686168602650ea82360f947fe2",
+    "url": "/pruebareact/index.html"
+  },
+  {
+    "revision": "28b1ee1e751bcfab7007",
+    "url": "/pruebareact/static/js/2.5ad21b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pruebareact/static/js/2.5ad21b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71252add71a1d2e92b9f",
+    "url": "/pruebareact/static/js/main.4ace11ef.chunk.js"
+  },
+  {
+    "revision": "dc69f0e593ff3143d9a6",
+    "url": "/pruebareact/static/js/runtime-main.d69f67f3.js"
+  }
+]);
